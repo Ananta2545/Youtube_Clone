fbf7e6f7d5deffd284580d05de5f245fff4e202b
SrijitaBaksi <baksisrijita@gmail.com>
2024-05-11 02:15:56 +0530

    added video page

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var menu_icon = document.querySelector('.menu-icon');
 var side_bar = document.querySelector('.side-bar');
+var container= document.querySelector('.container')
 
 menu_icon.onclick = function(){
     side_bar.classList.toggle("small-side-bar");
+    container.classList.toggle("large-container");
 }
+
+
+
+
 
